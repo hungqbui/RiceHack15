373321193d9353a0861e97985c9fe3d81e0d0e25
npu6703 <phuonguyen672003@gmail.com>
2025-09-20 22:07:58 -0500

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.tsx';
-import Login from './pages/Login.tsx';
-import ProtectedRoute from './components/ProtectedRoute.tsx';
-import { AuthProvider } from './contexts/AuthContext.tsx';
-import QuizExam from './pages/QuizExam.tsx';
-import { QuizContextProvider } from './contexts/QuizContext.tsx';
 
 function App() {
   return (
